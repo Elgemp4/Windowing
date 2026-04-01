bc38838abdfd49b37942d26f79776eaa7b529107
Emilien Marquegnies <marquegniesemilien@gmail.com>
2026-04-02 01:04:42 +0200

    Fix : linear minimum logic error

--- src/main/java/be/groupe18/windowing/strategies/minimum/LinearMinimumStrategy.java
+++ src/main/java/be/groupe18/windowing/strategies/minimum/LinearMinimumStrategy.java
@@ -6,10 +6,10 @@ import java.util.function.BiFunction;
 public class LinearMinimumStrategy<T> implements MinimumStrategy<T> {
     @Override
     public int getMinimum(List<T> elements, BiFunction<T, T, Boolean> greaterThan, int start, int end) {
-        T minElement = elements.getFirst();
-        int minIndex = 0;
+        T minElement = elements.get(start);
+        int minIndex = start;
 
-        for (int i = start; i < end; i++) {
+        for (int i = start ; i < end; i++) {
             T e = elements.get(i);
             if(greaterThan.apply(minElement, e)){
                 minElement = e;
